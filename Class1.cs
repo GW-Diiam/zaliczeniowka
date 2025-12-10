72c5bf62bac706fa04ca99e510e9a9f469305d0a
GW-Diiam <rafingpl@gmail.com>
2025-12-10 13:16:16 +0100

    na wszelki dodaje pliki osobno

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaliczeniowka
+{
+    class Class1
+    {
+        public string Imie { get; set; }
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+
+        public Class1(string imie, string nazwa, decimal cena)
+        {
+            Imie = imie;
+            Nazwa = nazwa;
+            Cena = cena;
+        }
+
+        public override string ToString()
+        {
+            return $"{Imie} - {Nazwa} - {Cena} zł";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zaliczeniowka
+{
+    public partial class Form1 : Form
+    {
+        private List<Class1> prezenty = new List<Class1>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void OdświeżWidok()
+        {
+            listBox1.Items.Clear();
+
+            foreach (var p in prezenty)
+            {
+                listBox1.Items.Add(p.ToString());
+            }
+
+            label1.Text = "Ilość: " + prezenty.Count;
+            label2.Text = "Łączny koszt: " + prezenty.Sum(p => p.Cena) + " zł";
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string imie = textBox1.Text;
+            string nazwa = textBox2.Text;
+
+            if (!decimal.TryParse(textBox3.Text, out decimal cena))
+            {
+                MessageBox.Show("Podaj poprawną cenę.");
+                return;
+            }
+            prezenty.Add(new Class1(textBox1.Text, textBox2.Text, cena));
+            OdświeżWidok();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
